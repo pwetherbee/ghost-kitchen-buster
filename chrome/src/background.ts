5564fd818d4b51d935530c09de0f752442ac2da4
Patrick Wetherbee <pwetherb96@gmail.com>
2024-05-10 12:05:42 -0500

    batching requests

--- chrome/src/background.ts
+++ chrome/src/background.ts
@@ -10,29 +10,41 @@ export function setupOnInstalledListener(): void {
 // This function should be called only once in the lifecycle of your background script.
 setupOnInstalledListener();
 // background.ts
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   if (request.restaurantNames) {
-    // Assuming your server endpoint is set up to receive POST requests at this route
-    fetch(`${DOMAIN}/api/check`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ restaurants: request.restaurantNames }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // data should be an object with restaurant names as keys and boolean as values indicating ghost kitchen status
-        console.log("Received ghost kitchen data:", data);
+    const batchSize = 15;
+    const restaurantNames = request.restaurantNames;
 
-        // Send data back to the content script
+    // Function to process each batch
+    async function processBatch(batch: string[]) {
+      try {
+        const response = await fetch(`${DOMAIN}/api/check`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ restaurants: batch }),
+        });
+        const data = await response.json();
+        console.log("Received ghost kitchen data for batch:", data);
+
+        // Send data back to the content script for each batch
         chrome.tabs.sendMessage(sender.tab?.id!, { ghostKitchenData: data });
-      })
-      .catch((error) => {
-        console.error("Error fetching ghost kitchen data:", error);
-        sendResponse({ error: "Failed to fetch ghost kitchen data" });
-      });
+      } catch (error) {
+        console.error("Error fetching ghost kitchen data for a batch:", error);
+        sendResponse({
+          error: "Failed to fetch ghost kitchen data for a batch",
+        });
+      }
+    }
 
+    // Create batches and process them
+    for (let i = 0; i < restaurantNames.length; i += batchSize) {
+      const batch = restaurantNames.slice(i, i + batchSize);
+      await processBatch(batch); // Wait for the current batch to be processed before moving to the next
+    }
+
+    sendResponse({ message: "All batches processed" });
     return true; // Indicates you wish to send a response asynchronously
   }
 });
